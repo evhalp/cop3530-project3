adf631deab76999e89eb1aad4bb8846246ad851b
Evan <evanhalperin@ufl.edu>
2025-07-27 11:51:46 -0400

    Create Dijkstra header file

---
+++ backend/include/Dijkstra.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <queue>
+#include "AdjacencyList.h"
+
+class Dijkstra {
+
+  private:
+
+    struct Node {
+      int station_id;
+      double travel_time;
+
+      Node(int id, double time) : station_id(id), travel_time(time) {}
+    };
+
+    std::array<std::string, 3> composite_key_;
+    AdjacencyList* adj_lists_;
+
+    // Returns the adjacency list keyed to the composite_key_
+    std::unordered_map<int, std::vector<Edge>>* GetAdjacencyList() const;
+    // Helper function for GetQuickestPath
+    // Relaxes the edge between two stations
+    void Dijkstra::relaxEdge(int from_id, int to_id, double edge_weight,
+                            std::unordered_map<int, double>& times,
+                            std::unordered_map<int, int>& predecessors,
+                            std::priority_queue<Node, std::vector<Node>, std::greater<>>& pq);
+    // Helper function for GetQuickestPath
+    // Uses predecessors list to get the quickest path found by Dijkstra algorithm
+    std::vector<Edge>* GetPath(std::unordered_map<int, int> predecessors, int start_id, int end_id) const;
+
+  public:
+
+    explicit Dijkstra(AdjacencyList* adj_lists)
+      : composite_key_({}), adj_lists_(adj_lists) {}
+
+    // Runs the Dijkstra Search algorithm using the stored adjacency list keyed to the composite_key_
+    std::pair<double, std::vector<Edge>> GetQuickestPath(const Station& start_station, const Station& end_station);
+
+    void SetCompositeKey(const std::array<std::string, 3>& composite_key) {composite_key_ = composite_key;}
+    std::array<std::string, 3> GetCompositeKey() const { return composite_key_; }
+
+    ~Dijkstra();
+
+};
--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -3,20 +3,27 @@
 #include <vector>
 
 #include "../include/AdjacencyList.h"
-//#include "../include/Dijkstra.h"
+#include "../include/Dijkstra.h"
 #include "../include/AStar.h"
 
 int main() {
     AdjacencyList adj_list;
-    Dijkstra dijkstra;
-    AStar a_star;
+    Dijkstra dijkstra(&adj_list);
+    //AStar a_star;
     adj_list.LoadFromCSV("../../data/subway_travel_times.csv");
 
-    // Get stations from front end
-    Station station1, station2;
+    // The above code runs at the initialization of the program
+    // The below code runs every time the user requests a route
 
-    auto dijkstra_output = dijkstra.GetOutput(adj_list, station1, station2);
-    auto a_star_output = a_star.GetOutput(adj_list, station1, station2);
+    // Get stations and composite key from front end
+    Station start_station, end_station;
+    std::array<std::string, 3> composite_key;
+
+    dijkstra.SetCompositeKey(composite_key);
+    //a_star.SetCompositeKey(composite_key);
+
+    std::pair<double, std::vector<Edge>> dijkstra_output = dijkstra.GetQuickestPath(start_station, end_station);
+    //auto a_star_output = a_star.GetOutput(adj_list, station1, station2);
 
     // Send time & edges to front end
 }
