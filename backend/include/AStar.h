bbff367cdf678edd0503505117f35ea2101754e4
LiamBeaubien <166727968+LiamBeaubien@users.noreply.github.com>
2025-08-02 15:47:35 -0400

    AStar Changes
    Adjusted and implemented similar functions and structure as Dijkstra

--- backend/include/AStar.h
+++ backend/include/AStar.h
@@ -5,29 +5,42 @@
 #include <unordered_map>
 #include <vector>
 #include <cmath>
-#include <limits>
 
 using namespace std;
 
 class AStar {
-public:
-    // Result: pair of <path as station IDs, total travel time>
-    static pair<double, vector<Station>> FindPath(const AdjacencyList& graph, const unordered_map<int, vector<Edge>>& adj_list,
-        int start_id, int goal_id);
-
 private:
     struct Node {
         int station_id;
-        double g_cost; // Cost from start to current node
-        double f_cost; // g + heuristic
+        double g_cost;
+        double f_cost;
 
         bool operator>(const Node& other) const {
             return f_cost > other.f_cost;
         }
     };
 
-    static double Heuristic(const Station& a, const Station& b);
-};
+    array<string, 3> composite_key_; //composite with month, time, week
+    AdjacencyList* adj_lists_;
 
+
+    
+    unordered_map<int, vector<Edge>>* adj_list_; 
+
+    unordered_map<int, vector<Edge>>* GetAdjacencyList() const;
+    double Heuristic(const Station& a, const Station& b) const; //calculation from the longitude and latitude
+
+    //same as get path from dijkstra class
+    vector<Station> ReconstructPath(unordered_map<int, int>& predecessors, int start_id, int end_id) const;
+
+public:
+    explicit AStar(AdjacencyList* adj_lists)
+        : composite_key_({}), adj_lists_(adj_lists), adj_list_(nullptr) {}
+//gets the adj list of composite key
+    void SetCompositeKey(const array<string, 3>& key);
+
+
+    pair<double, vector<Station>> GetQuickestPath(const Station& start_station, const Station& end_station);
+};
 
 
--- backend/src/AStar.cpp
+++ backend/src/AStar.cpp
@@ -1,23 +1,57 @@
-#include <algorithm> // for reverse
-#include <queue>
-
 #include "../include/AStar.h"
+#include <algorithm>
+
 using namespace std;
 
-double AStar::Heuristic(const Station& a, const Station& b) {//just the pythag of the coordinates of each station passed
+unordered_map<int, vector<Edge>>* AStar::GetAdjacencyList() const {
+    return adj_lists_->GetAdjacencyList(composite_key_);
+}
+
+void AStar::SetCompositeKey(const array<string, 3>& key) {
+    composite_key_ = key;
+    adj_list_ = adj_lists_->GetAdjacencyList(key);
+}
+
+//euclidian distance 
+double AStar::Heuristic(const Station& a, const Station& b) const {
     double dx = a.coordinates.first - b.coordinates.first;
     double dy = a.coordinates.second - b.coordinates.second;
     double distance = sqrt(dx * dx + dy * dy); // in degrees
-
-    double time_minutes = distance * 111.0/ 30.0* 60.0;           // degrees to km to hours to minutes
-
-    return time_minutes;
+    return distance * 111.0 / 28.0 * 60.0; // estimate in minutes
 }
 
-pair<double, vector<Station>> AStar::FindPath(    const AdjacencyList& graph,
-    const unordered_map<int, vector<Edge>>& adj_list,
-    int start_id,
-    int goal_id) {
+//uses the map to get path from start id to finish id
+vector<Station> AStar::ReconstructPath(unordered_map<int, int>& predecessors, int start_id, int end_id) const {
+    vector<int> path_ids;
+    int node = end_id;
+    while (predecessors.find(node) != predecessors.end()) {
+        path_ids.push_back(node);
+        node = predecessors[node];
+    }
+    path_ids.push_back(start_id);
+    reverse(path_ids.begin(), path_ids.end());
+
+    //id to station object
+    vector<Station> path;
+    for (int id : path_ids) {
+        const Station* s = adj_lists_->GetStation(id);
+        if (s) path.push_back(*s);
+    }
+    return path;
+}
+
+//same as the original findpath, gets hitorical data and heuristic
+pair<double, vector<Station>> AStar::GetQuickestPath(const Station& start_station, const Station& end_station) {
+    if (!adj_list_) return {-1.0, {}};
+
+
+    //initialization of id, and search structures
+    int start_id = adj_lists_->GetStationId(start_station);
+    int end_id = adj_lists_->GetStationId(end_station);
+
+    if (start_id == -1 || end_id == -1) {
+        return {-1.0, {}};
+    }
 
     priority_queue<Node, vector<Node>, greater<Node>> open_set;
     unordered_map<int, double> g_cost;
@@ -25,41 +59,32 @@ pair<double, vector<Station>> AStar::FindPath(    const AdjacencyList& graph,
 
     g_cost[start_id] = 0.0;
 
-    const Station* start_station = graph.GetStation(start_id);
-    const Station* goal_station = graph.GetStation(goal_id);
-    double h_start = Heuristic(*start_station, *goal_station);
+    const Station* goal_ptr = adj_lists_->GetStation(end_id);
+    double h_start = Heuristic(start_station, *goal_ptr);
     open_set.push({start_id, 0.0, h_start});
 
+
+
+    //A* logic
     while (!open_set.empty()) {
         Node current = open_set.top();
         open_set.pop();
-
-        if (current.station_id == goal_id) {
-            // Reconstruct path
-            vector<Station> path;
-            int node = goal_id;
-            while (came_from.find(node) != came_from.end()) {
-                const Station* s = graph.GetStation(node);
-                if (s) path.push_back(*s);
-                node = came_from[node];
-            }
-            const Station* s = graph.GetStation(start_id);
-            if (s) path.push_back(*s);
-
-            reverse(path.begin(), path.end());
-            return {g_cost[goal_id], path};
+        //final loop
+        if (current.station_id == end_id) {
+            return {g_cost[end_id], ReconstructPath(came_from, start_id, end_id)};
         }
-
-        for (const Edge& edge : adj_list.at(current.station_id)) {
-            int neighbor_id = graph.GetStationId(edge.end_station);
+            //checks the neighboring nodes
+        for (const Edge& edge : (*adj_list_)[current.station_id]) {
+            int neighbor_id = adj_lists_->GetStationId(edge.end_station);
             double tentative_g = g_cost[current.station_id] + edge.travel_time;
 
+            //compares the neighbor node time to best
             if (!g_cost.count(neighbor_id) || tentative_g < g_cost[neighbor_id]) {
                 g_cost[neighbor_id] = tentative_g;
-
-                const Station* neighbor_station = graph.GetStation(neighbor_id);
-                double h = Heuristic(*neighbor_station, *goal_station);
-                double f = tentative_g + h;
+                
+                const Station* neighbor_station = adj_lists_->GetStation(neighbor_id);
+                double h = Heuristic(*neighbor_station, *goal_ptr);
+                double f = tentative_g + h; //combines the historical and heuristic 
 
                 open_set.push({neighbor_id, tentative_g, f});
                 came_from[neighbor_id] = current.station_id;
