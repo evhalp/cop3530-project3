c56cc3e3f30450dad57a352f47d108b49caf3e13
Jennifer <jenniferklaman@gmail.com>
2025-08-03 00:01:06 -0400

    will update filepaths to access docs and repo in the am. also small ui fixes in the am. im tired sorry.

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/jenni/School/DSA/P3/cop3530-project3/backend
-# Build directory: C:/Users/jenni/School/DSA/P3/cop3530-project3/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/jenni/School/DSA/P3/cop3530-project3/build/Tests-b12d07c_include.cmake")
-subdirs("_deps/catch2-build")
